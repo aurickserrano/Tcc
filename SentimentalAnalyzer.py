e36b056c248f795ad4fa2ccf4fb1f467058e996e
Aurick <aurickserrano@hotmail.com>
2023-09-22 17:35:31 -0300

    codigo_clean

--- SentimentalAnalyzer.py
+++ SentimentalAnalyzer.py
@@ -1,110 +1,73 @@
+# Consolidando todo o código em uma célula para fácil referência
 
+# Importações
 import pandas as pd
+from textblob import TextBlob
 import matplotlib.pyplot as plt
-import nltk
-from nltk.corpus import wordnet
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk import word_tokenize, pos_tag
-from nltk.stem import WordNetLemmatizer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import seaborn as sns
+from collections import Counter
 
+# Carregar dados
+# df = pd.read_csv('/mnt/data/Olympics_Tokyo_tweets.csv')  # Já carregado anteriormente
 
+# Tratamento de texto (remoção de caracteres especiais e emoticons)
+def clean_text(text):
+    import re
+    return re.sub(r'[^\w\s]', '', text)
 
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('punkt')
-nltk.download('tagsets/pt_core_web_sm')
-nltk.download('vader_lexicon')
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('stopwords')
-nltk.download('tagsets/pt_core_web_sm')
-nltk.download('averaged_perceptron_tagger')
+# df['cleaned_text'] = df['text'].apply(clean_text)  # Já feito anteriormente
 
-ds = pd.read_csv('base_de_dados\Olympics_Tokyo_tweets.csv',low_memory=False)
+# Classificação por categoria (em inglês)
+english_keywords = {
+    'Opening_Ceremony': ['opening', 'ceremony', 'start', 'ateez', 'ateezofficial'],
+    'Sports_Events': ['athletics', 'swimming', 'gymnastics', 'competition', 'event',
+                      'archery', 'artistic gymnastics', 'artistic swimming', 'athletics', 'badminton',
+                      'baseball', 'softball', 'basketball', 'beach volleyball', 'boxing', 'canoe slalom',
+                      'canoe sprint', 'cycling bmx freestyle', 'cycling bmx racing', 'cycling mountain bike',
+                      'cycling road', 'cycling track', 'diving', 'equestrian', 'fencing', 'football', 'golf',
+                      'handball', 'hockey', 'judo', 'karate', 'marathon swimming', 'modern pentathlon',
+                      'rhythmic gymnastics', 'rowing', 'rugby sevens', 'sailing', 'shooting', 'skateboarding',
+                      'sport climbing', 'surfing', 'swimming', 'table tennis', 'taekwondo', 'tennis',
+                      'trampoline gymnastics', 'triathlon', 'volleyball', 'water polo', 'weightlifting', 'wrestling'],
+    'Results_and_Medals': ['medal', 'gold', 'silver', 'bronze', 'winner'],
+    'Controversies_and_Scandals': ['controversy', 'scandal', 'doping'],
+    'Media_Coverage': ['media', 'coverage', 'news']
+}
 
-sid = SentimentIntensityAnalyzer()
+def classify_tweet_english(tweet):
+    for category, keys in english_keywords.items():
+        if any(key in tweet.lower() for key in keys):
+            return category
+    return 'Others'
 
-tweets = ds['text'].tolist()
+# df['category_english_updated'] = df['cleaned_text'].apply(classify_tweet_english)  # Já feito anteriormente
 
-Analise = {'Positivo': 0, 'Neutro': 0, 'Negativo': 0}
-
-nome_arquivo_csv = "C:/Users/auric/Documents/Tcc/base_de_dados/Olympics_Tokyo_tweets.csv"
-
-
-
-def extrair_assunto(frase):
-
-    lemmatizer = WordNetLemmatizer()
-    tokens = word_tokenize(frase) 
-    tags = pos_tag(tokens) 
-    print(tags)
-    subject = [ lemmatizer.lemmatize(word, pos='v') for word, tag in tags if tag.startswith("VB") or tag == "NN"] 
-    print(subject)
-
-    if len(subject) >= 1: 
-      subject = subject[0]
-
+# Análise de sentimento com limites ajustados
+def adjusted_sentiment_analysis(text):
+    analysis = TextBlob(text)
+    polarity = analysis.sentiment.polarity
+    if polarity > 0.2:
+        return 'Positive'
+    elif polarity < -0.2:
+        return 'Negative'
     else:
-      subject = None
+        return 'Neutral'
 
-    return subject
+# Amostra de 51% dos dados
+df_sample_51 = df_cleaned.sample(frac=0.51, random_state=42)
 
-def processar_csv(nome_arquivo_csv):
-    df = pd.read_csv(nome_arquivo_csv, low_memory=False)
-    assuntos_principais = []
+# Aplicar análise de sentimento
+df_sample_51['adjusted_sentiment'] = df_sample_51['cleaned_text'].apply(adjusted_sentiment_analysis)
 
-    for index, row in df.iterrows():
-        texto = row['text']
-        assunto_principal = extrair_assunto(texto)
-        assuntos_principais.append(assunto_principal)
+# Agrupar por categoria e sentimento
+adjusted_sentiment_by_category_51 = df_sample_51.groupby(['category_english_updated', 'adjusted_sentiment']).size().reset_index(name='count')
 
-    return assuntos_principais
-
-
-assuntos_principais = processar_csv(nome_arquivo_csv)
-
-for tweet in tweets:
-    texto = tweet
-    resultado = sid.polarity_scores(texto)
-    polaridade = resultado['compound']
-    if polaridade > 0.5:
-      Analise['Positivo'] += 1
-    elif polaridade > -0.5 and polaridade < 0.5:
-      Analise['Neutro'] += 1
-    else:
-      Analise['Negativo'] += 1
-
-for i, assunto in enumerate(assuntos_principais):
-    print(f"Linha {i+1}: {assunto}")
-
-
-Analise_Resultado = list(Analise.keys())
-Analise_Valor = list(Analise.values())
-
-df = pd.DataFrame({
-   'Tweets': tweets,
-   'Assunto Principal': assuntos_principais
-})
-
-df['Sentimento Positivo'] = Analise['Positivo']
-df['Sentimento Neutro'] = Analise['Neutro']
-df['Sentimento Negativo'] = Analise['Negativo']
-df.to_csv('analise_tweets.csv', index=False)
-
-
-# Cria um gráfico de dispersão das polaridades e subjetividades
-# plt.bar(Analise_Resultado, Analise_Valor, color = 'Maroon', width = 0.4)
-# plt.title('Análise sentimental dos tweets')
-# plt.xlabel('Classificação')
-# plt.ylabel('Numero de resultados')
-# plt.show()
-
-# plt.pie(Analise_Valor, labels = Analise_Resultado)
-# plt.show()
-
-
-
-
-
+# Gráficos
+categories = adjusted_sentiment_by_category_51['category_english_updated'].unique()
+for category in categories:
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='adjusted_sentiment', y='count', data=adjusted_sentiment_by_category_51[adjusted_sentiment_by_category_51['category_english_updated'] == category])
+    plt.title(f'Adjusted Sentiment Analysis for {category} (51% Sample)')
+    plt.xlabel('Sentiment')
+    plt.ylabel('Count')
+    plt.show()
--- teste.py
+++ teste.py
@@ -1,11 +1,51 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# Dados de exemplo
-x = [1, 2, 3, 4, 5]
-y = [10, 20, 15, 30, 25]
+import nltk
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# Cria um gráfico de barras
-plt.bar(x, y)
+# Baxiando as bibloetcas que iremos utilizar
+nltk.download('vader_lexicon')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('tagsets/pt_core_web_sm')
+nltk.download('averaged_perceptron_tagger')
 
-# Exibe o gráfico
+sid = SentimentIntensityAnalyzer()
+
+# Cria uma lista para armazenar as frases filtradas
+ds = pd.read_csv('base_de_dados\Olympics_Tokyo_tweets.csv' ,low_memory=False )
+tweets = ds['text'].tolist()
+
+# Cria listas para armazenar as polaridades e subjetividades
+Analise = {'Positivo': 0, 'Neutro': 0, 'Negativo': 0}
+
+# Realiza a análise sentimental dos tweets e adiciona as polaridades e subjetividades às listas
+for tweet in tweets:
+    texto = tweet
+    resultado = sid.polarity_scores(texto)
+    polaridade = resultado['compound']
+    if polaridade > 0.5:
+      Analise['Positivo'] += 1
+    elif polaridade > -0.5 and polaridade < 0.5:
+      Analise['Neutro'] += 1
+    else:
+      Analise['Negativo'] += 1
+
+Analise_Resultado = list(Analise.keys())
+Analise_Valor = list(Analise.values())
+
+# Cria um gráfico de dispersão das polaridades e subjetividades
+plt.bar(Analise_Resultado, Analise_Valor, color = 'Maroon', width = 0.4)
+plt.title('Análise sentimental dos tweets')
+plt.xlabel('Classificação')
+plt.ylabel('Numero de resultados')
+plt.show()
+
+plt.pie(Analise_Valor, labels = Analise_Resultado)
 plt.show()
